39d7cbc6da559f7f3373522a77d9e2ea8f9afe91
Tundephilps <tundephilps@gmail.com>
2024-06-10 16:57:37 +0100

    User Interface Build

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import Login from "./Pages/Login";
 import Chat from "./Pages/Chat";
 import SignUp from "./Pages/SignUp";
 import Modal from "./components/Chat/Modal";
+import SpinningLoader from "./components/Loader/SpinningLoader";
+import ChatLoader from "./components/Loader/ChatLoader";
 
 function App() {
   return (
     <>
       {/* <Login /> */}
       {/* <SignUp /> */}
-      <Chat />
+      {/* <Chat /> */}
       {/* <Modal /> */}
+      {/* <SpinningLoader /> */}
+      {/* <ChatLoader /> */}
     </>
   );
 }
