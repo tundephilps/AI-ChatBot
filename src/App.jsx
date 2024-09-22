1758df5577694902238921d26309adb943924f2e
Tunde philps <90014837+tundephilps@users.noreply.github.com>
2024-09-23 00:13:19 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
 import Login from "./Pages/Login";
-import Dashboard from "./Pages/Dashboard";
+import Chat from "./Pages/Chat";
+import SignUp from "./Pages/SignUp";
+import withAuth from './middleware';
+
+const AuthenticatedChat = withAuth(Chat);
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
 
-          <Route path="/Dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
+  <Router>
+      <Routes>
+         <Route path="/" element={<Login />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="/signup" element={<SignUp />} />        
+         <Route path="/chat" element={<AuthenticatedChat />} />
+
+      </Routes>
+    </Router>
     </>
   );
 }
---
+++ src/components/Chat/Conversation.jsx
@@ -0,0 +1,77 @@
+import avatar from "../../assets/images/avatar.png";
+import { format } from 'date-fns';
+import avatar2 from "../../assets/images/avatar2.png";
+import PropTypes from 'prop-types';
+import SpinningLoader from "./SpinningLoader";
+import { useEffect, useState } from "react";
+
+
+
+
+
+const Conversation = ({conversation,isLoading, error, chatRef,isSuccess}) => {
+
+  const [currentDateTime, setCurrentDateTime] = useState(format(new Date(), 'MMM dd, HH:mm a'));
+
+  //console.log(conversation)
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDateTime(format(new Date(), 'MMM dd, HH:mm'));
+    }, 1000 * 60); // Update every minute
+
+    return () => clearInterval(interval);
+  }, []);
+
+
+  return (
+    <div className="w-full p-4 h-[60vh] overflow-y-auto" >
+      {/* <p className="text-xs text-center mb-4">Jan 28, 21:23PM</p> */}
+      <p className="text-xs text-center mb-4">{currentDateTime} </p>
+     {isLoading ? <SpinningLoader /> : null }
+
+     {error ? <p className="text-red-400 text-xs">Error: Failed to Loading</p> : null }
+
+      {
+       isSuccess && conversation.length > 0 ?  conversation.map((item,index) => {
+            return item.user_id == null ? (
+              <div className="flex flex-row gap-3 mt-2" key={index}>
+            <img src={avatar} className="rounded-full h-8 w-8" />
+
+            <div className="p-2 bg-[#F0F9FF] rounded-full ">
+              <p className="text-xs text-[#15ABFF]">{item.content}</p>
+            </div>
+          </div>
+
+            ): (
+              <div className=" flex justify-end mt-2" key={index}>
+              <div className="flex flex-row gap-3">
+                <div className="p-2 bg-[#6D31ED] rounded-full ">
+                  <p className="text-xs  text-white">
+                    {item.content}
+                  </p>
+                </div>
+      
+                <img src={avatar2} className="rounded-full h-8 w-8" />
+              </div>
+            </div>
+            );
+        }): (<p className="text-xs text-center">Chat is Empty</p>) 
+      }
+
+      <div ref={chatRef}></div>
+
+    </div>
+  );
+};
+
+Conversation.propTypes = {
+  conversation: PropTypes.array,
+  isLoading: PropTypes.bool.isRequired,
+  error: PropTypes.object,
+  chatRef: PropTypes.object,
+  isSuccess: PropTypes.bool,
+};
+
+export default Conversation;
